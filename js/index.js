c7ebd673564682a10b5aaa5c69480644ea99f1a4
邓佳艳 <1143463819@qq.com>
2016-06-13 17:18:17 +0800

    ff

--- js/index.js
+++ js/index.js
@@ -1,8 +1,13 @@
-$(function(){
-	$(function($){
-         	$('#fullpage').fullpage({
-         		 
-         	});
-         })
+$(function($){
+	$('#fullpage').fullpage({
+
+	});
+	var mySwiper = new Swiper('.swiper-container',{
+		slidesPerView : 3,
+		slidesPerGroup : 3,
+		prevButton:'.swiper-button-prev',
+        nextButton:'.swiper-button-next',
+	})
 })
-         
+
+
